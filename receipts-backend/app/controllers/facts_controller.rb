e63351da852d3568f3f5f11de936b9879f5a9c8d
sydnee510 <sbsydnee161416@gmail.com>
2020-09-15 13:25:19 -0700

    build fact controller

--- receipts-backend/app/controllers/facts_controller.rb
+++ receipts-backend/app/controllers/facts_controller.rb
@@ -1,2 +1,34 @@
 class FactsController < ApplicationController
+   
+    before_action :set_myth
+   
+    def index
+        @facts = @myth.facts
+        render json: @facts
+    end
+
+    def create 
+        @fact = @fact.myths.new(fact_params)
+        @fact = Fact.new(fact_params)
+        if @fact.save 
+            render json: @fact
+        else
+            render json: {error: 'Error creating Fact'}
+        end
+    end 
+
+    def show 
+        @fact = Fact.find(params[:id])
+        render json: @fact
+    end
+
+    private
+
+    def set_myth
+        @myth = Myth.find(params[:myth_id])
+    end 
+
+    def fact_params 
+        params.require(:fact).permit(:true_content, :receipt_url, :myth_id)
+    end
 end
