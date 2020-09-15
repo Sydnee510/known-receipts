f2977a51c816f4c61c507c2d4ff76f3dc06357de
sydnee510 <sbsydnee161416@gmail.com>
2020-09-15 13:25:03 -0700

    add nested facts resource

--- receipts-backend/config/routes.rb
+++ receipts-backend/config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :myths do 
     resources :facts 
   end
-  resources :facts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
