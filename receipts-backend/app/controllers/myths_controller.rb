7a5d928eb48b35b6a763c8d4ad6745bb3260ff76
sydnee510 <sbsydnee161416@gmail.com>
2020-09-15 12:28:57 -0700

    build myth controller

--- receipts-backend/app/controllers/myths_controller.rb
+++ receipts-backend/app/controllers/myths_controller.rb
@@ -1,2 +1,31 @@
 class MythsController < ApplicationController
+    def index
+        @myths = Myth.all
+        render json: @myths
+    end 
+
+    def create 
+        @myth = Myth.new(myth_params)
+        if @myth.save 
+            render json: @myth
+        else
+            render json: {error: 'Error creating Myth'}
+        end
+    end 
+
+    def show 
+        @myth = Myth.find(params[:id])
+        render json: @myth
+    end
+
+    def destroy 
+        @myth = Myth.find(params[:id])
+        @myth.destroy
+    end 
+
+    private
+
+    def myth_params 
+        params.require(:myth).permit(:false_content)
+    end
 end
