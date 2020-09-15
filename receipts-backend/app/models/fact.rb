2edf0b57f4e2b98ab5d79f5c98edf7cf681a31d9
sydnee510 <sbsydnee161416@gmail.com>
2020-09-15 10:10:54 -0700

    update models with associactions/validations

--- receipts-backend/app/models/fact.rb
+++ receipts-backend/app/models/fact.rb
@@ -1,2 +1,4 @@
 class Fact < ApplicationRecord
+    has_many :myths
+    validates :true_content, :receipt_url, presence: true
 end
--- receipts-backend/app/models/myth.rb
+++ receipts-backend/app/models/myth.rb
@@ -1,2 +1,4 @@
 class Myth < ApplicationRecord
+    belongs_to :fact
+    validates :false_content, presence: true
 end
