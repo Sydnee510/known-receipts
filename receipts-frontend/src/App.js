e501d0c856d038b53d963786c258ab22c6eab546
sydnee510 <sbsydnee161416@gmail.com>
2020-09-15 14:10:14 -0700

    update app.js to make fetch request for myths

--- receipts-frontend/src/App.js
+++ receipts-frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+  componentDidMount() {
+    fetch('http://localhost:3000/myths', {
+      method: "GET"
+    })
+
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  }
   render() {
     return (
       <div className="App">
-       App
+      <h1>Receipts For Kamala Harris</h1> 
       </div>
     );
   }
