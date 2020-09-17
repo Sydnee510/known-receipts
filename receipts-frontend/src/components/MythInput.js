3dba9b09a742588497ee3b046d20e0457789376a
sydnee510 <sbsydnee161416@gmail.com>
2020-09-16 19:57:47 -0700

    set up myth input file

---
+++ receipts-frontend/src/components/MythInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class MythInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default MythInput
