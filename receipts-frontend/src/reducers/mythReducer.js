bb3801d1469786015effdcffd777d35c208ed7f9
sydnee510 <sbsydnee161416@gmail.com>
2020-09-18 01:23:06 -0700

    add a new case 'add_myth' to reducer

--- receipts-frontend/src/reducers/mythReducer.js
+++ receipts-frontend/src/reducers/mythReducer.js
@@ -3,6 +3,8 @@ export default function mythReducer(state = {myths: []}, action) {
     switch (action.type){
         case 'FETCH_MYTHS':
             return {myths: action.payload}
+        case 'ADD_MYTH':
+            return {...state, myths: [...state.myths, action.payload]}
         default:
             return state
     }
