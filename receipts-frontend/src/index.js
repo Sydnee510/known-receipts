319fa46eb4ba8839853cd4ab2c059783146066d5
sydnee510 <sbsydnee161416@gmail.com>
2020-09-19 13:30:28 -0700

    wrapped app in router so all have access to routes

--- receipts-frontend/src/index.js
+++ receipts-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import mythReducer from './reducers/mythReducer'
 
 import App from './App';
@@ -14,7 +15,9 @@ let store = createStore(mythReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
      <App />
+     </Router>
   </Provider>,
   document.getElementById('root')
 );
