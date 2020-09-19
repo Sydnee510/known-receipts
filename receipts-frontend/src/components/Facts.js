e547355a38164b5269ebbfcc32b82999df1f4c00
sydnee510 <sbsydnee161416@gmail.com>
2020-09-19 15:25:36 -0700

    create facts.js & factsInput.js

---
+++ receipts-frontend/src/components/Facts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Facts = (props) => {
+
+    return (
+        <div>
+            Facts
+        </div>
+    )
+}
+
+export default Facts
---
+++ receipts-frontend/src/components/FactsInput.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+class FactsInput extends React.Component {
+    render(){
+        return(
+            <div>
+                Facts Input
+            </div>
+        )
+    }
+}
+export default FactsInput
