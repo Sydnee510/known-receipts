2f03071843d5c60baa79a6269f77c7bd21015935
sydnee510 <sbsydnee161416@gmail.com>
2020-09-19 15:23:45 -0700

    add switch component to assist with routing

--- receipts-frontend/src/containers/MythsContainer.js
+++ receipts-frontend/src/containers/MythsContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import {fetchMyths} from '../actions/fetchMyths'
 import Myths from '../components/Myths'
 import Myth from '../components/Myth'
@@ -14,9 +14,11 @@ class MythsContainer extends React.Component {
     render() {
         return (
             <div>
+                <Switch>
                 <Route path='/myths/new' component={MythInput}/>
                 <Route path='/myths/:id' render={(routerProps) => <Myth {...routerProps} myths={this.props.myths}/>}/>
-                <Route exact path='/myths' render={(routerProps) => <Myths {...routerProps} myths={this.props.myths}/>}/> 
+                <Route path='/myths' render={(routerProps) => <Myths {...routerProps} myths={this.props.myths}/>}/> 
+                </Switch>
             </div>
         )
     }
