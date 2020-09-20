ead042a6a7ea5b84bfe66475f0be7b90f222af52
sydnee510 <sbsydnee161416@gmail.com>
2020-09-19 17:38:29 -0700

    build fact form and handle functions

--- receipts-frontend/src/components/FactsInput.js
+++ receipts-frontend/src/components/FactsInput.js
@@ -1,12 +1,42 @@
 import React from 'react' 
+import {connect} from 'react-redux'
+import {addFact} from '../actions/addFact'
 
 class FactsInput extends React.Component {
+    
+    state = {
+        true_content: '',
+        receipt_url: ''
+    }
+    
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value 
+         })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+         this.props.addFact(this.state, this.props.myth.id)
+        alert("Fact Created!")
+        this.setState({
+            true_content: '',
+            receipt_url: ''
+        })
+    }
+    
+    
     render(){
         return(
             <div>
-                Facts Input
+                <form onSubmit={this.handleSubmit}>
+                    <label>Fact Content:</label>
+                    <input type="text" placeholder="Add a new fact" value={this.state.true_content} name="true_content" onChange={this.handleChange}/>
+                    <input type="text" placeholder="receipt url" value={this.state.receipt_url} name="receipt_url" onChange={this.handleChange}/>
+                <input type="submit"/>
+                </form>
             </div>
         )
     }
 }
-export default FactsInput
+export default connect(null, {addFact})(FactsInput)
