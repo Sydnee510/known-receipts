782661c45fee7de2d243610fb575286442561254
sydnee510 <sbsydnee161416@gmail.com>
2020-09-20 12:43:19 -0700

    add links to homepage for routes

--- receipts-frontend/src/App.js
+++ receipts-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 //import {connect} from 'react-redux'
+import { Link } from 'react-router-dom'
 import MythsContainer from './containers/MythsContainer'
  
 class App extends React.Component {
@@ -9,6 +10,9 @@ class App extends React.Component {
     return (
       <div className="App">
       <h1>Receipts For Kamala Harris</h1> 
+      <Link to="/">Home Page</Link> <br></br>
+      <Link to="/myths">Rumors</Link> <br></br>
+      <Link to="/myths/new">+Rumor</Link> <br></br>
       <MythsContainer/>
       </div>
     );
