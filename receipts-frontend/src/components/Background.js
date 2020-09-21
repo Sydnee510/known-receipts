86b953cb1cdef76a0002fa428648ec660ba44ab3
sydnee510 <sbsydnee161416@gmail.com>
2020-09-20 22:38:57 -0700

    add kamalas background

--- receipts-frontend/src/components/Background.js
+++ receipts-frontend/src/components/Background.js
@@ -4,6 +4,15 @@ const Background = () => {
     return (
     <div className='kamalas-background'>
         <h4> Kamala's Background</h4>
+        <p>
+            She was born in Oakland, Ca in 1964 of african and south asain decent. <br></br> <br></br>
+            She served as a Deputy District Attorney from about 1990-1998 in Oakland, Ca. <br></br> <br></br>
+            She served as District Attorney in San Francisco from about 2004-2010. <br></br> <br></br>
+            She served as California's Attorney General from about 2011-2017. <br></br> <br></br>
+            She was then elected to U.S. Senate as a Democrat in 2016 and began her first term 
+            representing California that following year. <br></br> <br></br>
+            She is now running for Vice President alongside Joe Biden. <br></br> <br></br>
+        </p>
     </div>
 
     )
