4b046567071b25d80f079d920a5c3ef1cf0a6922
sydnee510 <sbsydnee161416@gmail.com>
2020-09-20 22:39:37 -0700

    make each receipt a link for each fact

--- receipts-frontend/src/components/Facts.js
+++ receipts-frontend/src/components/Facts.js
@@ -5,7 +5,7 @@ const Facts = (props) => {
     return (
         <div>
             {props.facts && props.facts.map(fact => 
-            <li key={fact.id}>{fact.true_content} - {fact.receipt_url}</li>
+            <li key={fact.id}>{fact.true_content} - <a href={fact.receipt_url}>Receipt</a></li>
             )}
         </div>
     )
