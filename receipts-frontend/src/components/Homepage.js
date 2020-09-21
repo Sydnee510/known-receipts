9f3bef73839a3898d1bb160d242210c7b37fce37
sydnee510 <sbsydnee161416@gmail.com>
2020-09-20 23:22:39 -0700

    add images and update favicon icon

--- receipts-frontend/src/components/Homepage.js
+++ receipts-frontend/src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React from 'react' 
-
+import Logo from '../photos/Screen Shot 2020-09-20 at 10.43.40 PM.png'
 const Homepage = () => {
     return (
     <div> 
@@ -7,6 +7,7 @@ const Homepage = () => {
             Did she do it or not? Is she the one to blame? 
             <br></br>Find out Today!!
         </h1>
+        <img src={Logo} alt="kamala"></img>
        
     </div>
     )
--- receipts-frontend/src/components/Myth.js
+++ receipts-frontend/src/components/Myth.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Redirect} from 'react-router-dom'
+//import {Redirect} from 'react-router-dom'
 // import {connect} from 'react-redux'
 // import {deleteMyth} from '../actions/deleteMyth'
 import FactsContainer from '../containers/FactsContainer'
