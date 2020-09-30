657d60191fee5a5b1bd26f6c4e9ba16bfdecf978
sydnee510 <sbsydnee161416@gmail.com>
2020-09-30 11:58:54 -0700

    refactor: change NavBar from PureComponent to component

--- receipts-frontend/src/components/NavBar.js
+++ receipts-frontend/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {connect} from 'react-redux'
 
-class NavBar extends React.PureComponent {
+class NavBar extends React.Component {
     componentDidMount(){
     }
     
