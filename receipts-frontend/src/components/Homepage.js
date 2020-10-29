f363beae8c9bfe0bac1e9c95b71221b102f43bc8
sydnee510 <sbsydnee161416@gmail.com>
2020-10-28 19:01:19 -0700

    take out extra words from homepage

--- receipts-frontend/src/components/Homepage.js
+++ receipts-frontend/src/components/Homepage.js
@@ -3,9 +3,8 @@ import Logo from '../photos/Screen Shot 2020-09-20 at 10.43.40 PM.png'
 const Homepage = () => {
     return (
     <div className="home-page"> 
-        <h1>It's almost election time!!! <br></br>
-            Did she do it or not? Is she the one to blame? 
-            <br></br>Find out Today!!
+        <h1>
+            KAMALA HARRIS
         </h1>
         <img src={Logo} alt="kamala" className="center"></img>
        
